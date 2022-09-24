53b1e0364dae104fa9d59bd29845f593f69eff1b
egorn <egor.nazarenko31@mail.ru>
2022-09-24 20:49:18 +0300

    Ввод логики для нашего проекта

--- WinFormsAppIF/Form1.cs
+++ WinFormsAppIF/Form1.cs
@@ -11,5 +11,25 @@ namespace WinFormsAppIF
         {
 
         }
+        public class Logic
+        {
+            public static string Compare(int value)
+            {
+                string outMessage = "";
+                if (value <= 0)
+                {
+                    outMessage = $"Значение x равно {value}";
+                }
+                else if (value > 0 && value <= 1)
+                {
+                    outMessage = $"Значение x равно введённому числу {value}";
+                }
+                else
+                {
+                    outMessage = $"Значение x равно квадрату введённого числа {value * value}";
+                }
+                return outMessage;
+            }
+        }
     }
 }
